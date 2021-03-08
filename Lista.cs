433f9099dcb794851cc76c51444728c0d545b1f7
Paulo Menegasso <paulomen.pm@gmail.com>
2021-03-08 10:08:37 -0300

    Inserted Methods Remove and RemoveMany

--- Lista.cs
+++ Lista.cs
@@ -3,44 +3,49 @@ using System.Collections;
 
 namespace Program {
     public class Lista {
-
         private int ListSize { get; set; }
         private string[] ListaTotal { get; set; }
         private static int Position { get; set; }
         public Lista() {
-            ListaTotal = new String[100];
+            ListaTotal = new String[5];
             Position = 0;
         }
 
         public void Add(string element){
             ListaTotal[Position] = element;
             Position ++;
+			VerifySize(Position);
         }
 
         public void AddMany(string[] elements) {
             
             foreach(string element in elements) {
-                ListaTotal[Position] = element;
-                Position ++;
+				Add(element);
             }
-
         }
+
         public void Remove(int position) {
-            var oldPosition = Position;
-            Position = position;
-            ListaTotal[Position] = "";
-            Position = oldPosition ++;
-        }
+			string[] NovaLista = new string[ListaTotal.Length];
+			for (int i = 0; i < position; i++) {
+				NovaLista[i] = ListaTotal[i];
+			}
+			for (int i = position; i < ListaTotal.Length -1; i++) {
+				NovaLista[i] = ListaTotal[i+1];
+			}
+			ListaTotal = NovaLista;
+		}
 
-        public void RemoveAll() {
-            Position = 0;
-            foreach(string element in ListaTotal) {
-                ListaTotal[Position] = "";
-                Position++;
-            }
-            Position = 0;
-        }
+		public void Where() {}
 
+        public void RemoveMany(int[] itensToRemove) {
+			foreach (int item in itensToRemove) {
+				Remove(item);
+
+				for (int i = 0; i < itensToRemove.Length; i++){
+					itensToRemove[i]--;
+				}
+			}
+		}
 
         public override string ToString() {
             string returnString = "";
@@ -51,5 +56,16 @@ namespace Program {
                 }
             return returnString;
         }
+
+		public void VerifySize(int Position) {
+			if (Position == ListaTotal.Length - 1) {
+				string[] NovaLista = new string[2*ListaTotal.Length];
+				for (int i = 0; i != ListaTotal.Length; i++){
+					//ListaTotal.CopyTo(NovaLista, i);
+					NovaLista[i] = ListaTotal[i];
+				}
+				ListaTotal = NovaLista;
+			}
+		}
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,15 +15,18 @@ namespace Program
             Lista.AddMany(arrayDeStrings);
 
             Console.WriteLine(Lista);
+			int[] itensToRemove = {0, 3};
+            Lista.RemoveMany(itensToRemove);
 
-            Lista.Remove(0);
-            Console.WriteLine(Lista);
+			
+            // Lista.Add("Hello 2");
+             Console.WriteLine(Lista);
 
-            Lista.Remove(3);
-            Console.WriteLine(Lista);
+            // Lista.Remove(3);
+            // Console.WriteLine(Lista);
 
-            Lista.RemoveAll();
-            Console.WriteLine(Lista);
+        //     Lista.RemoveAll();
+        //     Console.WriteLine(Lista);
         }
     }
 }
